abbae752325089787cdae8bfd475fdfcc57806cd
IgorGultyaev <igor.gultyaev@mail.ru>
2022-02-13 13:45:24 +0300

    реформат кода

--- src/com/company/ValueOutOfRangeException.java
+++ src/com/company/ValueOutOfRangeException.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class ValueOutOfRangeException extends RuntimeException {
-    public ValueOutOfRangeException(String value_out_of_range) {
-        super(value_out_of_range);
+    public ValueOutOfRangeException(String valueOutOfRange) {
+        super(valueOutOfRange);
     }
 }
